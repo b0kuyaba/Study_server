f6d20e0a235d4938cf4047a954d2e2f5fb081b76
b0kuyaba <e10yc1122@gmail.com>
2026-08-18 11:33:18 +0900

    문제 5 풀이

--- src/chapter2/OperationAssignment5.java
+++ src/chapter2/OperationAssignment5.java
@@ -1,4 +1,10 @@
 package chapter2;
 
 public class OperationAssignment5 {
+    public static void main(String[] args) {
+        int x = 17;
+
+        String result = (x % 2 == 1 ? "홀수" : "짝수");
+        System.out.println(result);
+    }
 }
