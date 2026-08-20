0d0343be5eb2ed8398002e26d962d70a90de71da
b0kuyaba <e10yc1122@gmail.com>
2026-08-20 15:23:26 +0900

    ConditionEx9 완료

--- src/Ifexample/ConditionEx7.java
+++ src/Ifexample/ConditionEx7.java
@@ -2,16 +2,26 @@ package Ifexample;
 
 public class ConditionEx7 {
     public static void main(String[] args) {
-        int month = 8;
-        String season = "";
+        int num1 = 20;
+        int num2 = 5;
+        char op = '/';
 
-        switch (month) {
-            case 3, 4, 5 -> season = "봄";
-            case 6, 7, 8 -> season = "여름";
-            case 9, 10, 11 -> season = "가을";
-            case 12, 1, 2 -> season = "겨울";
-            default -> season = "잘못된 입력";
-        }
-        System.out.println(season + "입니다.");
-    };
+        switch (op) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                } else {
+                    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                }
+
+            default:
+                System.out.println(num1 + " " + num2 + " = " + (num1 * num2));
+        };
+    }
 }
