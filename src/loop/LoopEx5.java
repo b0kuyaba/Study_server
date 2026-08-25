cb2c913f9fbf3503e277ed187dd3dcae631dd525
b0kuyaba <e10yc1122@gmail.com>
2026-08-25 12:35:20 +0900

    LoopEx5 완료

--- src/loop/LoopEx5.java
+++ src/loop/LoopEx5.java
@@ -3,10 +3,11 @@ package loop;
 public class LoopEx5 {
     public static void main(String[] args) {
         int i,j;
+        int num = 1;
 
-        for ( i = 1; i <= 5; i++) {
-            for (j=1; j <= i; j++) {
-                System.out.print('*');
+        for ( i = 0; i <= 4; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(num++ + " ");
             }
             System.out.println();
         }
