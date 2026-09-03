8158cd510932f4b747dd92f799618c291f46b205
b0kuyaba <e10yc1122@gmail.com>
2026-09-03 15:02:43 +0900

    ArrayEx6 수정

--- src/array/ArrayEx6.java
+++ src/array/ArrayEx6.java
@@ -4,13 +4,16 @@ public class ArrayEx6 {
     public static void main(String[] args) {
         int [][] score = new int[][]{{89,76,100,68,48,98,56,77,95},{50,60,70,100,99,88,83,78,93}};
         int sum = 0;
+        double avg = 0;
 
         for (int i = 0; i < score.length; i++) {
             sum = 0;
             for (int j = 0; j < score[i].length; j++) {
                 sum += score[i][j];
-
             }
+            avg = (double)sum / score[i].length;
+            System.out.printf("%c반 평균: %.1f", 'A' + i,avg);
+            System.out.println();
         }
     }
 }
