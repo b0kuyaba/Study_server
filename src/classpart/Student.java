c1bb8c399e8a1000f25a3640b7f5e51b17af800d
b0kuyaba <e10yc1122@gmail.com>
2026-09-10 14:10:28 +0900

    class

--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -6,6 +6,28 @@ public class Student {
     int grade;
     String address;
 
+    public String getStudentName(){
+        return studentName;
+    }
+
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentName = "이순신";
+
+        Student studentAhn = new Student();
+        studentAhn.studentName = "안중근";
+
+        Student studentKim = new Student();
+        studentKim.studentName = "김좌진";
+
+        System.out.println(studentLee.getStudentName());
+        System.out.println(studentAhn.getStudentName());
+        System.out.println(studentKim.getStudentName());
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
     public void showStudentInfo() {
         System.out.println(studentName + "," + address);
     }
